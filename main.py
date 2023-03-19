33f0e612bde96461c6713c0c11b9fb3842876c4f
AliShaban1 <127267687+AliShaban1@users.noreply.github.com>
2023-03-19 04:02:35 -0400

    commiting reader

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from kivy.app import App
-from kivy.uix.widget import Widget
-
-class MainWidget(Widget):
-    pass
-
-
-class TheُTrackerApp(App):
-    pass
+import csv
+with open("dataset/CO2_passenger_cars_v12.csv", 'r') as file:
+    reader = csv.DictReader(file)
+    dataset = dict()
+    for row in file:
+        print(row)
+        # dataset[(row[15], row[16], row[19])] = row[24]
 
 
-TheُTrackerApp().run()
+class CarInfo:
+    def __init__(self, make, model, engine_type):
+        self.make = make
+        self.model = model
+        self.engine_type = engine_type
+
+
+# make = input("Please enter your car's make")
